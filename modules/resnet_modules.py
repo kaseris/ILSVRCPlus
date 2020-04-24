48c28dbb83faf548100c3a536ad52a7aff625ef0
kaseris <mich.kaseris@gmail.com>
2020-04-24 23:36:27 +0300

    resnet modules

--- modules/resnet_modules.py
+++ modules/resnet_modules.py
@@ -16,12 +16,14 @@ class ResNetModules:
 
 	@staticmethod
 	def resnet_layer(input_tensor, num_filters=16, kernel_size=3, strides=1, activation='relu', batch_normalization=True, conv_first=True):
+		
 		conv_tensor = Conv2D(filters=num_filters,
 		kernel_size=kernel_size,
 		strides=strides,
 		padding='same',
 		kernel_initializer='he_normal',
 		kernel_regularizer=l2(1e-4))
+		
 		x = input_tensor
 		if conv_first:
 			x = conv_tensor(x)
@@ -34,7 +36,7 @@ class ResNetModules:
 				x = BatchNormalization()(x)
 			if activation is not None:
 				x = Activation(activation)(x)
-			x = conv(x)
+			x = conv_tensor(x)
 	
 		return x
 	
